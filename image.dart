1fac70919bd6040afdb0d034022f132e14d6e11f
Durkesh Kumaaran <durkesh250998@gmail.com>
2020-10-04 14:28:45 +0530

    Create image.dart

---
+++ image.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Hello There !'),
+        ),
+        body: new Container(
+            child: Image(
+          image: AssetImage('asset/mountain.jpg'),
+        )));
+  }
+}
+// DONT FORGET TO ADD IMAGE PATH TO pubspec.yaml in assets
